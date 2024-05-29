2ca2c589cb0cf65d1cad440c4bcb02649f46f985
IrenaMartty <irenamartincevic3@gmail.com>
2024-05-29 22:46:04 +0200

    mensaje de Ire

--- src/components/IreMsje.jsx
+++ src/components/IreMsje.jsx
@@ -1,6 +1,9 @@
 import { Typography } from "@mui/material";
 
+
+
 const IreMsje = () => {
+  
     return (
       <Typography
       style={{
@@ -9,6 +12,14 @@ const IreMsje = () => {
         color: "black",
       }}
     >
+Menos mal que el Guille del pasado, quien pensaba que nunca llegaría a ser profesor, estaba equivocado. Para mí, contar con un buen mentor fue crucial, especialmente al llegar al final del curso. Agradezco toda la paciencia, dedicación y el amor que brindaste en cada clase, así como por creer en nosotras. Eres un crack 👨🏻‍💻 y me siento muy afortunada de tenerte como mi profe. Te deseo todo lo mejor en el futuro.
+<br/>
+<br/>
+Tambien, gracias a Ro, la futura profe 👩🏻‍💻, que siempre encuentra la manera facíl de explicar algo a nosotros "dummies" 🤪.
+<br/>
+<br/>
+Mil gracias a los dos 💞
+
 
     </Typography>
     )
