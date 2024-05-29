be2c32a37f55a7ca42286747ac74c2e1aff0a1b7
MariaSolGonzalez <mariasolgonzalez240@hotmail.com>
2024-05-29 17:53:07 -0500

    -

--- src/components/SolMsje.jsx
+++ src/components/SolMsje.jsx
@@ -9,16 +9,7 @@ const SolMsje = () => {
         color: "black",
       }}
     >
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
-      reprehenderit placeat eaque ab rem explicabo quidem tempore! Expedita
-      obcaecati quos quo cumque inventore doloremque, aspernatur libero aliquam
-      unde ipsum odit? Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-      Modi facilis veniam quo cumque aliquam odit? In sapiente fugit excepturi
-      est illum mollitia, sunt neque ex corporis nam. Quae, vitae accusantium?
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum ea sequi
-      sapiente facilis? Laborum fugiat saepe deserunt quisquam facilis eum
-      perspiciatis omnis sit quo hic? Temporibus totam voluptates suscipit.
-      Eaque?
+       
     </Typography>
   );
 };
