c5e31b308c58b9e18d791d53c08cd8c5a24ccc29
Angellysg <sepulveda.angelly1@gmail.com>
2024-05-29 15:21:55 -0300

    Mi mensaje , Angelly‍‍

--- src/components/AngellyMsje.jsx
+++ src/components/AngellyMsje.jsx
@@ -9,6 +9,18 @@ const AngellyMsje = () => {
         color: "black",
       }}
     >
+      Al momento de estudiar cualquier cosa, para mí es muy importante la persona que lo vaya a enseñar. Eso puede hacer la diferencia entre amar mucho el contenido o odiarlo, jeje. En este caso, con usted, Profe Guille, fue un aprendizaje muy ameno, ya que su manera de explicar es fácil de entender. Lo explica también de una manera como si amara programar, y eso se refleja en que también le agarremos cariño al contenido. Siempre está a disposición y siempre le agrega algo divertido y fresco a la clase.
+      <br />
+      Quiero agradecerle, ya que fue clave para mi aprendizaje sobre React. Aunque sé que me falta y que puedo seguir mejorando, su enseñanza fue clave para lograr tener una base sólida. Espero siga enseñando para que, así como a mí, nos ayude a querer aprender y querer la programación, por más dolores de cabeza que nos dé a veces programar.
+      <br />
+      Gracias, Profe Guille.💫
+      <br />
+      <br />
+      Nota: Gracia Ro tambien , por tu buena onda siempre y por aportar siempre comentarios de valor a la clase.💫
+      <br />
+      GRACIAS ❤️💫
+      <br />
+      Con Amor Angelly!👩‍💻
     </Typography>
   );
 };
